b3d2cec250c120c7d7171e87d1175e80b24595b0
s-joshid <j.dhruvi89@gmail.com>
2023-11-28 12:26:27 -0800

    fixed indentation issue in custom_mapping script

--- scripts/.ipynb_checkpoints/InterproScanRun-checkpoint.sh
+++
@@ -1,6 +0,0 @@
-date 
-./my_interproscan/interproscan-5.63-95.0/interproscan.sh --cpu 16 -i ./RandomizedReformatted.fasta -f tsv -goterms -b ./Transporter_proj/data/InterproRandomizedRun 2>&1 | tee log.txt
-date
-
-#to see progress:
-# cat subsetTest.tsv | cut -d $'\t'  -f 1 | sort -u | wc -l
